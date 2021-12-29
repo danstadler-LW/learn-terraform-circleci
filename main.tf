422a6a9057900d19fe29e04a00e928b53861497c
Dan Stadler <daniel.stadler@lacework.net>
2021-12-28 16:41:55 -0800

    small changes

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   required_version = "> 0.14"
   backend "s3" {
-    bucket = "circle-ci-backend-20211229003436776600000001"
+    bucket = "circle-ci-backend-20211229003952836400000001"
     key    = "terraform/webapp/terraform.tfstate"
     region = "us-west-2"
   }
